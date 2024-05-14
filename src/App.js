a8d19936606de14baa353f52ce8bd0fa61980c6e
ankit-suman-07 <ankitsuman07@gmail.com>
2024-05-15 01:12:42 +0530

    demo: first iteration tested

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    { task: "Task 1", selected: false },
+    { task: "Snorkelling", selected: false },
+    { task: "Scuba Dive", selected: false },
+    { task: "Task 19", selected: false },
+    { task: "New Task", selected: false }
+  ]);
+
+  const toggleSelected = (task) => {
+    setTasks(tasks.map((item) => {
+      if (task === item.task) {
+        return { ...item, selected: !item.selected };
+      }
+      return item;
+    }));
+  };
+
+  const deleteSelected = () => {
+    setTasks(tasks.filter(item => !item.selected));
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Taskify
+      <div>
+        <input type="text" placeholder="Enter Task" />
+        <button>Add</button>
+      </div>
+      <div>
+        {
+          // genders.forEach((gender) => console.log(gender));
+          tasks && tasks.map((item, index) => {
+            return (
+              <div key={index} >
+                <button>
+                  {
+                    item.selected ? <span>Selected</span> : <span>Not</span>
+                  }
+                </button>
+                {item.task}
+                <button onClick={() => toggleSelected(item.task)}>Toggle</button>
+              </div>
+            )
+
+          })
+        }
+      </div>
+      <button onClick={deleteSelected}>
+        Delete Tasks
+      </button>
     </div>
   );
 }
