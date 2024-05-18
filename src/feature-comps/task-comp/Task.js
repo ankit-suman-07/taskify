0b8125de648777acd79f3cd42c9a0da3fe860df5
ankit-suman-07 <ankitsuman07@gmail.com>
2024-05-19 02:17:13 +0530

    mod: todo tasks css modified

--- src/feature-comps/task-comp/Task.js
+++ src/feature-comps/task-comp/Task.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./Task.css";
 
+import TickIcon from "../../assets/tick.png";
+
 const Task = ({ name }) => {
     const [currentTask, setCurrentTask] = useState("");
     const [clicked, setClicked] = useState(false);
@@ -50,35 +52,38 @@ const Task = ({ name }) => {
 
     return (
         <div className="task-outer" >
-            <div>
-                name: {name}
+            <div className="task-header" >
+                {name}
             </div>
-            <div>
+            <div className="task-input" >
                 <input
                     type="text"
                     placeholder="Enter Task"
                     value={currentTask}
                     onChange={(event) => setCurrentTask(event.target.value)}
+
                 />
-                <button onClick={() => addTask(currentTask)}>Add</button>
+                <button onClick={() => addTask(currentTask)} className="task-input-add-btn" >
+                    Add
+                </button>
             </div>
             <div>
                 {
                     // genders.forEach((gender) => console.log(gender));
                     tasks && tasks.map((item, index) => {
                         return (
-                            <div key={index} >
-                                <button onClick={() => toggleSelected(item.task)}>
+                            <div key={index} className="task-individual-div" id={item.line ? "line-through-disable" : ""}>
+                                <button onClick={() => toggleSelected(item.task)} className="task-toggle-btn" >
                                     {
-                                        item.selected ? <span>Selected</span> : <span>Not</span>
+                                        item.selected ? <div className="toggle-selected" ></div> : <div className="toggle-not-selected"  ></div>
                                     }
                                 </button>
                                 <span className={item.line ? "line-through" : ""} >
                                     {item.task}
                                 </span>
 
-                                <button onClick={() => deleteOne(item, index)} >
-                                    x
+                                <button onClick={() => deleteOne(item, index)} className="task-complete-btn" >
+                                    <img src={TickIcon} alt="tick-icon" />
                                 </button>
                             </div>
                         )
