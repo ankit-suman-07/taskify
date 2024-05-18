57073d6a3c572f4640ca54aedae0a3be7b6685e8
ankit-suman-07 <ankitsuman07@gmail.com>
2024-05-18 22:02:21 +0530

    feat: floating menu icons implemented

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -9,9 +9,19 @@ import Navbar from '../../components/navbar/Navbar';
 import TaskGroup from '../../feature-comps/task-group/TaskGroup';
 import Main from "../../components/main-page/Main";
 
+import HomeIcon from "../../assets/home.png";
+import TodoIcon from "../../assets/todo.png";
+import NotesIcon from "../../assets/post-it.png";
+import DocsIcon from "../../assets/document.png";
+
 const Home = () => {
     const [feature, setFeature] = useState("main");
-
+    const components = {
+        "main": <Main />,
+        "task": <TaskGroup />,
+        "notes": <Notes />,
+        "docs": <Notes />
+    }
 
 
     return (
@@ -19,14 +29,36 @@ const Home = () => {
             <nav>
                 <Navbar />
             </nav>
-            <div>
-                <button onClick={() => setFeature("main")} >Main</button>
-                <button onClick={() => setFeature("task")} >Task btn</button>
-                <button onClick={() => setFeature("notes")} >Notes btn</button>
+            <div className='floating-menu-outer' >
+                <div className='floating-menu' >
+                    <button onClick={() => setFeature("main")}
+                        className='floating-menu-btn'
+                        id={feature === "main" ? "active-btn" : ""}>
+                        <img src={HomeIcon} alt='home-icon' />
+                    </button>
+                    <button onClick={() => setFeature("task")}
+                        className='floating-menu-btn'
+                        id={feature === "task" ? "active-btn" : ""}>
+                        <img src={TodoIcon} alt='todo-icon' />
+                    </button>
+                    <button onClick={() => setFeature("notes")}
+                        className='floating-menu-btn'
+                        id={feature === "notes" ? "active-btn" : ""}>
+                        <img src={NotesIcon} alt='notes-icon' />
+                    </button>
+                    <button onClick={() => setFeature("docs")}
+                        className='floating-menu-btn'
+                        id={feature === "docs" ? "active-btn" : ""}>
+                        <img src={DocsIcon} alt='document-icon' />
+                    </button>
+                </div>
             </div>
-            {
+            {/* {
                 feature == "main" ? <Main />
                     : (feature == "task" ? <TaskGroup /> : <Notes />)
+            } */}
+            {
+                components[feature]
             }
             <footer>
                 Footer
